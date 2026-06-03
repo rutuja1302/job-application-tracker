ca36e677c77dc1667f057915bfa31a9a3e2f3f34
Rutuja Mhatre <rutujamhatre48@gmail.com>
2026-06-03 18:43:33 +0930

    adding validations

--- backend/tracker/src/main/java/com/application/tracker/controller/UserController.java
+++ backend/tracker/src/main/java/com/application/tracker/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.application.tracker.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.application.tracker.Service.UserService;
 import com.application.tracker.entity.User;
 
+import jakarta.validation.Valid;
+
 @RestController
+@Validated
 public class UserController {
     @Autowired
     UserService userService;
 
     @PostMapping("/register")
-    public User registerUser(@RequestBody User user){
+    public User registerUser(@RequestBody @Valid User user){
         User registeredUser = userService.createNewUser(user);
         return registeredUser;
     }
--- backend/tracker/src/main/java/com/application/tracker/entity/User.java
+++ backend/tracker/src/main/java/com/application/tracker/entity/User.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity
@@ -15,7 +17,10 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message="User name is required")
     private String name;
+    @Email(message="Email format incorrect")
     private String email;
+    @NotBlank(message="Password is required")
     private String password;
 }
